165fdfdb7343dbdfab753949bfea77a0e35a712c
Cristian Tironi <tironi741@gmail.com>
2023-02-13 18:56:09 -0300

    FindByEmail
    Implementmaos el metodo findOneByEmail

--- src/presentation/controllers/customer/customer.controller.ts
+++ src/presentation/controllers/customer/customer.controller.ts
@@ -2,16 +2,23 @@ import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/
 import { CustomerService } from '../../../business';
 import { CustomerEntity} from '../../../data';
 import { UpdateCustomerDto } from '../../../business/dtos/update-customer.dto';
+import { CustomerRepository } from '../../../data/persistence/repositories/customer.repository';
 
 
 
 @Controller('customer')
 export class CustomerController {
-    constructor(private readonly customerService: CustomerService) { }
+    constructor(private readonly customerService: CustomerService,
+                private readonly customerRepository: CustomerRepository
+        ) { }
 
     @Get('getCustomer/:id')
     getCustomerInfo(@Param('id', ParseUUIDPipe) customerId: string): CustomerEntity {
         return this.customerService.getCustomerInfo(customerId);
+    }
+    @Get('getCustomerByEmail/:email')
+    getCustomerIByEmail(@Param('email') email: string): CustomerEntity {
+        return this.customerRepository.findOneByEmail(email);
     }
 
     @Get('getAll')
